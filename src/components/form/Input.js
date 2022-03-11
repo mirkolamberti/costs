5fa82e8809fedcddd9926ac86fa90eabcfcff731
Mirko Lamberti <mirkolamberti@gmail.com>
2022-03-10 21:38:33 -0300

    Aula 22 - Componentização formulário

---
+++ src/components/form/Input.js
@@ -0,0 +1,12 @@
+import styles from './Input.module.css'
+
+function Input({type, text, name, placeholder, handleOnChance, value}) {
+    return(
+        <div className={styles.form_control}>
+            <label htmlFor={name}>{text}</label>
+            <input type={type} name={name} placeholder={placeholder} onChange={handleOnChance} value={value} />
+        </div>
+    )
+}
+
+export default Input
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,21 +1,23 @@
-function ProjectForm() {
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}) {
     return(
-        <form>
+        <form className={styles.form}>
             <div>
-                <input type="text" placeholder="Insira o nome do projeto" />
+                <Input type={"text"} text={"Nome do projeto"} name={"nomePrj"} placeholder={"Insira o nome do projeto"}  />
             </div>
             <div>
-            <input type="number" placeholder="Insira o orçamento total" />
+                <Input type={"number"} text={"Orçamento do projeto"} name={"budget"} placeholder={"Insira o orçamento total"}  />
             </div>
             <div>
-            <select name="category_id">
-                <option disabled selected>
-                    Selecione a categoria
-                </option>
-            </select>
+                <Select name="category_id" text="Selecione a categoria" />
             </div>
             <div>
-            <input type="submit" value="Criar projeto" />
+                <SubmitButton text={btnText} />
             </div>
         </form>
     )
