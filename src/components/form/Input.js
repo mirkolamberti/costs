0d7cb45977f09d27660f6c277a0d70194ad66e26
Mirko Lamberti <mirkolamberti@gmail.com>
2022-03-10 21:45:58 -0300

    testes

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,6 +1,7 @@
 import styles from './Input.module.css'
 
 function Input({type, text, name, placeholder, handleOnChance, value}) {
+
     return(
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}</label>
