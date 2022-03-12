96f4bead714309aa5a9949d7053e15e67d167ddb
Mirko Lamberti <mirkolamberti@gmail.com>
2022-03-11 21:20:42 -0300

    Aula 23 - Conexão banco de dados! ProjectForm

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
@@ -5,6 +7,23 @@ import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+    
     return(
         <form className={styles.form}>
             <div>
@@ -14,7 +33,7 @@ function ProjectForm({btnText}) {
                 <Input type={"number"} text={"Orçamento do projeto"} name={"budget"} placeholder={"Insira o orçamento total"}  />
             </div>
             <div>
-                <Select name="category_id" text="Selecione a categoria" />
+                <Select name="category_id" text="Selecione a categoria" options={categories} />
             </div>
             <div>
                 <SubmitButton text={btnText} />
