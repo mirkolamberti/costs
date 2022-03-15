06771a5a490ee9888992b3c31bf78332456e80ab
Mirko Lamberti <mirkolamberti@gmail.com>
2022-03-14 21:35:59 -0300

    aula 27 - buscando dados e formatando resultados

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,13 +1,16 @@
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 import Message from "../layout/Message"
 import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
 
 import styles from "./Projects.module.css"
 
 function Projects(){
 
+    const [projects, setProjects] = useState([])
     const location = useLocation()
 
     let message = ''
@@ -15,6 +18,21 @@ function Projects(){
         message = location.state.message
     }
 
+    useEffect(() => {
+        
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setProjects(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
     return(
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -23,10 +41,34 @@ function Projects(){
             </div>
             {message && <Message type="success" msg={message} />}
             <Container customClass="start">
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                    projects.map((project) =>
+                        <ProjectCard 
+                            id={project.id}
+                            name={project.nomePrj}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    )
+                }
             </Container>
         </div>
     )
 }
 
 export default Projects
+
+
+/**
+                     projects.map((project) => 
+                        <ProjectCard 
+                            id={project.id}
+                            name={project.name} 
+                            budget={project.budget}
+                            category={project.category}
+                            key={project.id}
+                        />
+                    )
+
+ */
